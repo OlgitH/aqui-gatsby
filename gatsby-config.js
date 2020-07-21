b56d16fb8a7c301a59b0a1ed39125ba8accc2429
Oliver Berman <berman.oj@gmail.com>
2020-07-21 23:31:53 +0100

    changed favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@ require("dotenv").config({
 })
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Oliver Berman - Portfolio`,
+    description: `Porfolio of web development and design work.`,
+    author: `Oliver Berman`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
