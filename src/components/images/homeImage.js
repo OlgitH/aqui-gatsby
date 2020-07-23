eb64127534f40955e13d5d8af9239dffe7beab03
Oliver Berman <berman.oj@gmail.com>
2020-07-23 14:03:21 +0100

    ~

--- src/components/images/homeImage.js
+++ src/components/images/homeImage.js
@@ -1,8 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
-import { css } from "@emotion/core"
-import {isIE} from 'react-device-detect';
+
 
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
@@ -15,25 +14,7 @@ import {isIE} from 'react-device-detect';
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-const imageStyle = css`
-  margin: 0;
-  position: absolute;
-  top: 50%;
-  left: -50px;
-  transform: translateY(-50%);
-  -webkit-transform: translateY(-50%);
-  -moz-transform: translateY(-50%);
-  -ms-transform: translateY(-50%);
-  @media screen and (-ms-high-contrast: active), (-ms-high-contrast: none) {
-    top:0;
-  }
-`
 
-const IEStyle = css`
-  position:relative;
-  top:0;
-  left:0;
-`
 
 const Image = () => {
   const data = useStaticQuery(graphql`
@@ -49,7 +30,15 @@ const Image = () => {
   `)
 
   return (
-    <Img fixed={data.placeholderImage.childImageSharp.fixed} css={ isIE ? IEStyle : imageStyle } />
+    <Img fixed={data.placeholderImage.childImageSharp.fixed}
+      style={{
+        margin: "0",
+        position: "absolute",
+        top: "50%",
+        left: "-50px",
+        transform: "translateY(-50%)"
+      }}
+    />
   )
 }
 
