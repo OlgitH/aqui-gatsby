9f71349c638991297f6cec0ae6dfd848b29e18d2
Oliver Berman <berman.oj@gmail.com>
2020-07-23 15:10:27 +0100

    changed footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import Header from "./header"
 import Footer from "./footer"
 import Form from "./form/form"
-
+import styled from "@emotion/styled"
 import { css } from "@emotion/core"
 import { connect } from "react-redux"
 
@@ -21,6 +21,21 @@ const linkStyle = css`
   cursor:pointer;
 `
 
+const Button = styled.button`
+ background-color:transparent;
+ border:1px solid #222;
+ border-radius: 10px;
+ -webkit-border-radius: 10px;
+ cursor:pointer;
+ padding:6px 10px;
+ outline:0;
+ &:hover {
+   background-color:#f6f6f6;
+
+ }
+
+`
+
 const LO = ({ children, formVisible, toggleForm }) => {
 
   return (
@@ -29,9 +44,10 @@ const LO = ({ children, formVisible, toggleForm }) => {
       <div>
         <main>{children}</main>
         <Footer>
-          <a onClick={toggleForm} css={linkStyle}>Send a message</a>
-          <p>or call...</p>
-          <a href="tel:00447757067088" css={linkStyle}>+44 (0)7757 076088</a>
+          <Button onClick={toggleForm}>Send message via website</Button>
+          <p style={{fontSize:'0.8rem', marginBottom:0}}>or contact:</p>
+            <a href="mailto:berman.oj@gmail.com" css={linkStyle} style={{fontSize:'0.8rem'}}>berman.oj@gmail.com</a><br/>
+          <a href="tel:00447757067088" css={linkStyle} style={{fontSize:'0.8rem'}}>+44 (0)7757 076088</a>
         </Footer>
         <Form closeForm={toggleForm} isOpen={formVisible} />
       </div>
